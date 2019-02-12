19120774228531d5a666ff269c74f562abc40ac6
artemovsergey <artik3314@gmail.com>
2019-02-12 16:37:21 +0300

    update generators

--- genegators.py
+++ genegators.py
@@ -5,3 +5,31 @@ print(next(s)) # работа генерации
 print(list(s))  # превращение генератора в список
 
 print(iter([1,2,3])) # превращение списка в генератор)
+
+
+# Генератор это функция с ключевым словом yield
+
+# Это обычная функция
+def gen():
+    iter = 0
+    s = 10
+    while iter < s:
+        return iter
+        iter+=1
+    
+print(gen())
+print('------------')
+# Это генератор 
+def gen1():
+    iter = 0
+    s = 10
+    while iter < s:
+        yield iter
+        iter+=1
+    
+generator = gen1()  
+print(next(generator))
+print(next(generator))
+print(next(generator))
+print(next(generator))
+print(next(generator))
